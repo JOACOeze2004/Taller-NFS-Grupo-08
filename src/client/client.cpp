bb47b6bbd03b5f7343b75ba8800094f1f8812681
JOACOeze2004 <jmendeza@fi.uba.ar>
2025-10-24 21:40:30 -0300

    Agrego que el cliente reciba toda la data del server usando el hilo receiver.
    Lo malo, es que como me falta el sender, y seguimos leyendo por entrada estandar, que es bloqueante, llega la info por tandas, es decir que si toco la w, me llega toda la data que estaba en la queue del receiver, pero hasta que no toque otra tecla no me llega mas data del auto. Pero seguro se soluciona con el hilo del sender.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,6 +1,8 @@
 #include "client.h"
 #include "client_protocol.h"
 #include "../common/constants.h"
+#include "client_receiver.h"
+
 
 Client::Client(const std::string& host, const std::string& port)
     : host(host), port(port), client_socket(host.c_str(), port.c_str()) {}
@@ -9,37 +11,39 @@ void Client::run() {
     std::cout << "[CLIENT] Connected to " << host << ":" << port << std::endl;
 
     ClientProtocol protocol(client_socket);
-
-    std::string line;
-    while (std::getline(std::cin, line)) {
-        if (!line.empty() && line[0] == SERVER_CLOSE) {
-            break;
-        }
-        uint8_t cmd = 0;
-        switch (line[0]) {
-            case 'w':
-                cmd = SEND_ACCELERATE; 
-                break;
-            case 'a':
-                cmd = SEND_ROTATE_LEFT; 
-                break;
-            case 'd':
-                cmd = SEND_ROTATE_RIGHT; 
-                break;
-            case 's':
-                cmd = SEND_BRAKE; 
-                break;
-            default: continue;
-        }
-
-        protocol.send_byte(cmd);
-
-        CarDTO state = protocol.receive_car_state();
-        std::cout << "[CLIENT] Car x:" << state.x 
-                  << " y:" << state.y 
-                  << " vel:" << state.velocity 
-                  << " ang:" << state.angle << std::endl;
-    }
     
+    ClientReceiver receiver(protocol); //Supongo q esta mal q esto este aca y q hagamos start.
+    receiver.start();
+
+    bool running = true;
+    std::string line;
+    CarDTO state;
+
+    while (running) {
+        while (receiver.try_pop_car_state(state)) {
+            std::cout << "[CLIENT] Car x:" << state.x
+                      << " y:" << state.y
+                      << " vel:" << state.velocity
+                      << " ang:" << state.angle << std::endl;
+        }
+        if (std::getline(std::cin, line)) {
+            if (!line.empty() && line[0] == SERVER_CLOSE) {
+                running = false;
+                break;
+            }
+            uint8_t cmd = 0;
+            switch (line[0]) {
+                case 'w': cmd = SEND_ACCELERATE; break;
+                case 'a': cmd = SEND_ROTATE_LEFT; break;
+                case 'd': cmd = SEND_ROTATE_RIGHT; break;
+                case 's': cmd = SEND_BRAKE; break;
+                default: continue;
+            }
+            protocol.send_byte(cmd);
+        }
+    }
+
+    receiver.stop();
+    receiver.join();
     protocol.close();
 }
