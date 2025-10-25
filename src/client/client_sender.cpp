8ceffe0f19da01de4c079dde61cba618793d74fc
Arlia1 <arliayoel23@gmail.com>
2025-10-25 14:48:52 -0300

    Sender manda al protocolo desde la queue

--- src/client/client_sender.cpp
+++ src/client/client_sender.cpp
@@ -1,1 +1,20 @@
 #include "client_sender.h"
+#include "../common/constants.h"
+
+ClientSender::ClientSender(ClientProtocol& _protocol, Queue<Command>& _command_queue)
+    : protocol(_protocol), command_queue(_command_queue) {}
+
+void ClientSender::run() {
+    try {
+        while (should_keep_running()) {
+            Command cmd = command_queue.pop();
+            
+            protocol.send_byte(cmd.cmd);
+
+        }
+    } catch (const std::exception& e) {
+        this->stop();
+    }
+}
+
+ClientSender::~ClientSender() {}
