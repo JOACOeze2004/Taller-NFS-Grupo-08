9d9433bf057c7e4db161476fc87f78ae8d4e0e05
Arlia1 <arliayoel23@gmail.com>
2025-10-30 21:05:14 -0300

    "Agrego campos necesarios para utilizar el snapshot"

--- src/common/DTO.h
+++ src/common/DTO.h
@@ -3,10 +3,66 @@
 
 #include "car_DTO.h"
 #include <unordered_map>
+#include <vector>
 
 struct DTO {
     CarDTO own_car;
     std::unordered_map<int, CarDTO> cars;
+};
+
+struct Snapshot { //deberia ser una clase
+    enum class StateRunning state;
+    int position;
+    int cars_count;
+    int id;
+    std::vector<CarDTO> cars; // capaz es un map
+    enum class Map map;
+    enum class Upgrades upgrade;
+    bool upgradeable;
+    enum class CollisionType collision;
+    bool under_bridge;
+    CheckpointCoords checkpoint;
+    enum class TypeCheckpoint type_checkpoint;
+};
+
+enum class StateRunning {
+    IN_GAME,
+    FINISHED,
+    DEAD, 
+    TIMEOUTED
+};
+
+enum class Map {
+    SAN_ANDREAS,
+    VICE_CITY,
+    LIBERTY_CITY
+};
+
+enum class Upgrades {
+    NONE,
+    SPEED,
+    ACCELERATION,
+    HANDLING,
+    NITRO,
+    LIFE
+};
+
+enum class CollisionType {
+    NONE,
+    CHILL_COLLISION,
+    HEAVY_COLLISION
+    //ADD MORE COLLISSION TYPES IF NEEDED 
+};
+
+struct CheckpointCoords{
+    float x;
+    float y;
+};
+
+enum class TypeCheckpoint{
+    REGULAR,
+    FINAL,
+    FIRST
 };
 
 #endif  // TALLER_TP_DTO_H
