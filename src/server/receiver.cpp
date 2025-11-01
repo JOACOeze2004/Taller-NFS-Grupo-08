52eba7f54b18540c19236e096879b580055a20ac
JOACOeze2004 <jmendeza@fi.uba.ar>
2025-10-31 22:53:10 -0300

    Ahora reciver no pushea mas a la cola en comun (porque no la tiene mas), y delega al gameLoop el pushea del comando que le mandan.

--- src/server/receiver.cpp
+++ src/server/receiver.cpp
@@ -1,18 +1,19 @@
 #include "receiver.h"
 
-ClientReceiver::ClientReceiver(ServerProtocol& prot, Queue<ClientCommand>& queue, int id):
-        protocol(prot), command_queue(queue), reciver_id(id) {}
+ClientReceiver::ClientReceiver(ServerProtocol& prot, std::shared_ptr<Gameloop> game_loop, int id):
+        protocol(prot), game_loop(game_loop), reciver_id(id) {}
 
 void ClientReceiver::run() {
     while (should_keep_running()) {
         try {
             uint8_t request = protocol.receive_standar_command();
-            ClientCommand client_command{};
-            client_command.id = this->reciver_id;
-            client_command.cmd_struct.cmd = request;
-            command_queue.push(client_command);
-
-            if (request == 0) {
+            if (request != 0) {
+                ClientCommand client_command;
+                client_command.id = this->reciver_id;
+                client_command.cmd_struct.cmd = request;
+                game_loop->push_command(client_command);
+            }
+            else {
                 break;
             }
         } catch (const std::exception& e) {
