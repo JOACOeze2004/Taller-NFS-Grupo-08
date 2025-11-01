34c01852f08ae361acb184ab0f7829cfa699fa9d
JOACOeze2004 <jmendeza@fi.uba.ar>
2025-10-31 22:47:29 -0300

    Ahora handler crea el hilo de la partida, delegando a monitor la creacion de la partida y del seteo del id.
    Aunque esta mal que cree directamente la partida, ya que deberiamos hacer lo de los botones que si quierer unirse a una partida o si queire crearla, pero es por ahora esto.

--- src/server/client_handler.cpp
+++ src/server/client_handler.cpp
@@ -1,14 +1,14 @@
 #include "client_handler.h"
 
 #include "src/common/DTO.h"
+#include "receiver.h"
 
-ClientHandler::ClientHandler(Socket&& peer, Queue<ClientCommand>& commands, int _id):
+ClientHandler::ClientHandler(Socket&& peer,Monitor& monitor, int _id):
         peer(std::move(peer)),
         protocol(this->peer),
-        command_queue(commands),
         client_queue(CLIENT_QUEUE_LEN),
+        monitor(monitor),
         id(_id),
-        receiver(protocol, command_queue, this->id),
         sender(protocol, client_queue) {} 
 
 void ClientHandler::run() {
@@ -43,8 +43,14 @@ void ClientHandler::run() {
         std::cerr << "[SERVER " << id << "] Error sending init data: " << e.what() << std::endl;
         return;
     }
+    std::string g_id;
+    std::shared_ptr<Gameloop> game = monitor.create_game();
+    g_id = monitor.get_last_created_game_id();
+    set_game_id(g_id);
 
-    receiver.start();
+    game->start();
+    receiver = std::make_shared<ClientReceiver>(protocol, game, id);
+    receiver->start();
     sender.start();
 }
 
@@ -57,16 +63,20 @@ void ClientHandler::kill() {
     stop();
 
     client_queue.close();
-    receiver.stop();
+    receiver->stop();
     sender.stop();
     kill_threads();
 }
 
 void ClientHandler::kill_threads() {
-    receiver.join();
+    receiver->join();
     sender.join();
 }
 
-bool ClientHandler::is_dead() const { return !sender.is_alive() && !receiver.is_alive(); }
+bool ClientHandler::is_dead() const { return !sender.is_alive() && !receiver->is_alive(); }
+
+void ClientHandler::set_game_id(const std::string& _game_id) { game_id = _game_id; }
+
+const std::string& ClientHandler::get_game_id() const { return game_id; }
 
 ClientHandler::~ClientHandler() {}
