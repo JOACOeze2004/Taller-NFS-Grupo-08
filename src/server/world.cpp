ee627ce4728779bfa965f5b9618d62d605213deb
Lucaaesp <lespineyra@fi.uba.ar>
2025-11-03 19:51:27 +0000

    Agregados los mapas de san andreas y vice city

--- src/server/world.cpp
+++ src/server/world.cpp
@@ -35,40 +35,40 @@ void World::generate_map(std::string map_name) {
         map = YAML::LoadFile("../src/server/libertycity.yaml");
     }
     if (map_name == "San Andreas") {
-        map = YAML::LoadFile("libertycity.yaml");
+        map = YAML::LoadFile("../src/server/sanandreas.yaml");
     }
     if (map_name == "Vice City") {
-        map = YAML::LoadFile("libertycity.yaml");
+        map = YAML::LoadFile("../src/server/vicecity.yaml");
     }
 
-    std::vector<StaticBody> edificios;
+    std::vector<StaticBody> boxes;
     for (const auto& layer : map["layers"]) {
         if (layer["type"].as<std::string>() != "objectgroup") continue;
         for (const auto& obj : layer["objects"]) {
-            StaticBody e;
-            e.id = obj["id"].as<int>();
-            e.x = obj["x"].as<float>();
-            e.y = obj["y"].as<float>();
-            e.width = obj["width"].as<float>();
-            e.height = obj["height"].as<float>();
-            edificios.push_back(e);
+            StaticBody box;
+            box.id = obj["id"].as<int>();
+            box.x = obj["x"].as<float>();
+            box.y = obj["y"].as<float>();
+            box.width = obj["width"].as<float>();
+            box.height = obj["height"].as<float>();
+            boxes.push_back(box);
         }
     }
 
-    for (const auto& e : edificios) {
+    for (const auto& box : boxes) {
 
         b2BodyDef bodyDef = b2DefaultBodyDef();
         bodyDef.type = b2_staticBody;
 
-        bodyDef.position = { (e.x + e.width/2), (e.y + e.height/2) };
+        bodyDef.position = { (box.x + box.width/2), (box.y + box.height/2) };
 
         b2BodyId body = b2CreateBody(world, &bodyDef);
         b2Body_EnableContactEvents(body, true);
         b2Body_EnableHitEvents(body, true);
 
-        b2Polygon box = b2MakeBox(e.width/2, e.height/2);
+        b2Polygon b2box = b2MakeBox(box.width/2, box.height/2);
         b2ShapeDef shape_def = b2DefaultShapeDef();
-        b2ShapeId shape = b2CreatePolygonShape(body, &shape_def, &box);
+        b2ShapeId shape = b2CreatePolygonShape(body, &shape_def, &b2box);
         b2Shape_EnableContactEvents(shape, true);
         b2Shape_EnableHitEvents(shape, true);
     }
