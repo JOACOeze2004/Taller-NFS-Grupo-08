f9aced2228ae17895795177a2e565fcf278516c3
Arlia1 <arliayoel23@gmail.com>
2025-11-18 12:27:38 -0300

    Separo los eventos en teclado y mouse, agrego mapas para los botones y cheats, modularizacion, se borran printeos, ventaja del usar mapas, si el evento es de tipo teclado y no esta en el mapa me voy y no reviso nada mas. despues de estos cambios hay una mejoria en la fluidez de la pantalla

--- src/client/client_handler.h
+++ src/client/client_handler.h
@@ -30,6 +30,8 @@ class ClientHandler {
     InputParser& parser;
     std::unordered_map<SDL_Keycode, std::function<void()>> key_map;
     std::unordered_map<SDL_Keycode, bool> key_state;
+    std::unordered_map<ButtonType, int> button_command_map;
+    std::unordered_map<SDL_Keycode, int> cheat_command_map;
     std::vector<ClickableButton> buttons;
     bool mouse_was_down;
 
@@ -44,10 +46,15 @@ public:
 
 private:
     void process_event(const SDL_Event& event);
+    void process_mouse_event(const SDL_Event& event);
+    void process_keyboard_event(const SDL_Event& event);
     void process_mouse_click(int x, int y);
     void handle_button_action(ButtonType type);  
     void update();
     void initialize_key_map();
+    void initialize_button_map();
+    void initialize_cheat_map();
+    bool is_point_in_rect(int x, int y, const SDL_Rect& rect) const;
 };
 
 #endif  // TALLER_TP_CLIENT_HANDLER_H
