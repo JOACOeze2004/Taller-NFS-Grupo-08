6c42031d0100283f03656d8c96ef1e5f0579600f
JOACOeze2004 <jmendeza@fi.uba.ar>
2025-11-21 21:57:33 -0300

    Agrego clase results handler para gestionar los resultados entre carreras y al final de la partida.
    Mas que anda para descomprimir al gl de hacer todo.

---
+++ src/server/results_handler.cpp
@@ -0,0 +1,63 @@
+#include "results_handler.h"
+#include <ranges>
+
+void ResultsHandler::initialize_player(const int id){
+    player_total_times[id] = 0;
+}
+
+bool ResultsHandler::add_finished(const int id, const std::string& name, int time, int position) {
+    if (is_player_in_finished(name)) {
+        return false;
+    }
+    
+    finished.emplace_back(name, time, position);
+    player_total_times[id] += time;
+    return true;
+}
+
+bool ResultsHandler::add_dead(const int id, const std::string& name, int time, int max_time) {
+    if (is_player_in_deads(name)) {
+        return false;
+    }
+    
+    deads.emplace_back(name, time, -1);
+    player_total_times[id] += max_time;
+    return true;
+}
+
+bool ResultsHandler::is_player_in_finished(const std::string& name) const {
+    return std::ranges::any_of(finished,[&](const CarRacingInfo& p) { return p.name == name; });
+}
+
+bool ResultsHandler::is_player_in_deads(const std::string& name) const {
+    return std::ranges::any_of(deads,[&](const CarRacingInfo& p) { return p.name == name; });
+}
+
+FinalScoreList ResultsHandler::calculate_final_scores(const std::unordered_map<int, std::string>& names) const {
+    FinalScoreList results;
+    for (const auto& [id, total_time] : player_total_times) {
+        results.push_back({names.at(id), static_cast<float>(total_time), 0});
+    }
+    std::ranges::sort(results, [](const auto& a, const auto& b) { return a.time < b.time;});
+    for (size_t i = 0; i < results.size(); ++i) {
+        results[i].position = static_cast<int>(i + 1);
+    }
+    return results;
+}
+
+void ResultsHandler::reset_for_next_race() {
+    finished.clear();
+    deads.clear();
+}
+
+void ResultsHandler::reset_all() {
+    finished.clear();
+    deads.clear();
+    player_total_times.clear();
+}
+
+const std::vector<CarRacingInfo>& ResultsHandler::get_finished() const { return finished; }
+
+const std::vector<CarRacingInfo>& ResultsHandler::get_deads() const { return deads; }
+
+const std::map<int, long long>& ResultsHandler::get_total_times() const { return player_total_times; }
---
+++ src/server/results_handler.h
@@ -0,0 +1,36 @@
+#ifndef TALLER_NFS_GRUPO_08_RESULTS_HANDLER_H
+#define TALLER_NFS_GRUPO_08_RESULTS_HANDLER_H
+
+#include <vector>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include "../common/DTO.h"
+
+class ResultsHandler{
+private:
+    std::vector<CarRacingInfo> finished;
+    std::vector<CarRacingInfo> deads;
+    std::map<int, long long> player_total_times;
+public:
+    ResultsHandler() = default;
+    ~ResultsHandler() = default;
+
+    void initialize_player(const int id);
+    bool add_finished(const int id, const std::string& name, int time, int position);
+    bool add_dead(const int id, const std::string& name, int time, int max_time);
+    bool is_player_in_finished(const std::string& name) const;
+    bool is_player_in_deads(const std::string& name) const;
+    
+    FinalScoreList calculate_final_scores(const std::unordered_map<int, std::string>& names) const;
+    void reset_for_next_race();
+    void reset_all();
+    
+    const std::vector<CarRacingInfo>& get_finished() const;
+    const std::vector<CarRacingInfo>& get_deads() const;
+    const std::map<int, long long>& get_total_times() const;
+};
+
+
+#endif  // TALLER_NFS_GRUPO_08_RESULTS_HANDLER_H
