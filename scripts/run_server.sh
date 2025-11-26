6d76e5a8249efe4447687befe5f137e91cb89ff3
JOACOeze2004 <jmendeza@fi.uba.ar>
2025-11-26 16:32:26 -0300

    Ahora el script del server puede ejecutar valgrind.
    Solo neecsitas ponerle run_server --valgrind, y te corre el server con valgrind (sin --valgrind, anda sin el mismo).

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -6,6 +6,19 @@ export NEED_FOR_SPEED_SERVER_CONFIG_FILE="/etc/need_for_speed/server_config.yaml
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-PORT="${1:-8080}"
+USE_VALGRIND=false
+PORT=8080
 
-/usr/bin/$BIN_SERVER_NAME "$PORT"
+if [[ "$1" == "--valgrind" ]]; then
+    USE_VALGRIND=true
+    PORT="${2:-8080}"
+else
+    PORT="${1:-8080}"
+fi
+
+if $USE_VALGRIND; then
+    echo "[SERVER] Running under Valgrind..."
+    exec valgrind --leak-check=full --track-origins=yes --show-reachable=yes --error-exitcode=2 --show-leak-kinds=all --trace-children=yes /usr/bin/$BIN_SERVER_NAME "$PORT"
+else
+    exec /usr/bin/$BIN_SERVER_NAME "$PORT"
+fi
